4a1397794b56b9a27ed40e1acf2be16a4d8d051d
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-17 20:36:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,9 @@
 # Your code goes here!
+class Dog
+#Write a setter method, .name=, that allows you to give a dog a name.
+
+#Write a getter method, .name that returns an individual dog's name.
+
+#Write a method, .bark, that puts "woof!" when called on an instance of Dog.
+
+end
