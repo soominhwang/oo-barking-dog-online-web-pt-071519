86826c9c11571de5e63feb5df809238b6208a18a
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-17 20:39:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,7 @@ class Dog
   end
 #Write a getter method, .name that returns an individual dog's name.
   def name
-    dog_name
+    this_dogs_name
   end
 #Write a method, .bark, that puts "woof!" when called on an instance of Dog.
   def bark
